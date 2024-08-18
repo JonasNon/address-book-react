0252e495443f890fda39f375fd2d5130f54693f0
JonasNon <officialjonasn@gmail.com>
2024-08-17 22:46:48 -0500

    all requirements technically passed

--- src/UserCard.js
+++ src/UserCard.js
@@ -10,12 +10,17 @@ function UserCard (props) {
   return (
     <div id="main">
       {/* <p>{index}</p> */}
-      <p id="name" onClick={expandMe}>{userInfo.name.first} {userInfo.name.last}</p>
-      <p>{expanded ? <p>Phone #: {userInfo.cell}</p> : null}</p>
-      <p>{expanded ? <p>Age: {userInfo.dob.age}</p> : null}</p>
-      <p>{expanded ? <p>BirthDay: {userInfo.dob.date.slice(0,10)}</p> : null}</p>
-      <p>{expanded ? <p>City: {userInfo.location.city}</p> : null}</p>
-      <p>{expanded ? <p>Country: {userInfo.location.country}</p> : null}</p>
+      <p id="name">{userInfo.name.first} {userInfo.name.last}</p>
+      <button onClick={expandMe}>{expanded ? "Hide Details" : "Show Details"}</button>
+      <div id="expansion">
+        <img src={expanded ? userInfo.picture.thumbnail : null}></img>
+        <p>{expanded ? <p>Phone #: {userInfo.cell}</p> : null}</p>
+        <p>{expanded ? <p>Age: {userInfo.dob.age}</p> : null}</p>
+        <p>{expanded ? <p>BirthDay: {userInfo.dob.date.slice(0,10)}</p> : null}</p>
+        <p>{expanded ? <p>City: {userInfo.location.city}</p> : null}</p>
+        <p>{expanded ? <p>Country: {userInfo.location.country}</p> : null}</p>
+      </div>
+
 
     </div>
 
